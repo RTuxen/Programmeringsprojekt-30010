14a9e7a5c873771c65ef701281ed590959d50a40
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-02 12:45:19 +0200

    Create ansi.h

---
+++ ansi.h
@@ -0,0 +1,23 @@
+#ifndef _ANSI_H_
+#define _ANSI_H_
+
+#include "struct.h"
+
+#define ESC 0x1B
+
+void fgcolor(uint8_t foreground);
+void bgcolor (uint8_t background);
+void color (uint8_t foreground, uint8_t background);
+void resetbgcolor();
+void window (int8_t x1, int8_t y1, int8_t x2, int8_t y2, char str[], int8_t style);
+void clrscr();
+void clreol ();
+void gotoxy(int8_t x, int8_t y);
+void underline (int8_t on);
+void blink (int8_t on);
+void inverse (int8_t on);
+void box (int8_t x1, int8_t y1, int8_t x2, int8_t y2, int8_t style);
+void minibox (int8_t x1, int8_t y1, int8_t x2, int8_t y2, int8_t style, uint8_t n);
+
+
+#endif /* _ANSI_H_ */
