cf73b8e12e258c30c2559c7bcb934f913331b5b4
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-03 14:07:15 +0200

    Update gamePhysics.c

--- src/gamePhysics.c
+++ src/gamePhysics.c
@@ -6,19 +6,38 @@
 #include "struct.h"
 #include "gamePhysics.h"
 
-void update(struct ball_t * ball, int8_t hit,int8_t x1 , int8_t y1,int8_t x2,int8_t y2){
+void initbold (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy){
+ball->x = x;
+ball-> y = y;
+ball->vec.x=dx;
+ball->vec.y=dy;
+
+
+}
+
+void update(struct ball_t * ball,int8_t x1 , int8_t y1,int8_t x2,int8_t y2){
     gotoxy(ball->x,ball->y);
     printf(" ");
 
-        ball->x += ball->dx;
-        ball->y += ball->dy;
 
-    if (ball->x <= x1) {
-            ball->x += (x1-ball->x-ball->dx)-ball->dx -2;
-    }
-     if (ball->x >= x2) {
-            ball->x += (x2-ball->x-ball->dx)-ball->dx -1;
-    }
+        ball->x += ball->vec.x;
+        ball->y += ball->vec.y;
+
+
+        // Makes sure ball doesn't exceed bounds with larger vectors
+        if ( ball->x >= x2){
+            ball->x = x2-1;
+        }
+        if ( ball->x <= x1){
+            ball->x = x1+1;
+        }
+        if ( ball->y >= y2){
+            ball->y = y2-1;
+        }
+        if ( ball->y <= y1){
+            ball->y = y1+1;
+        }
+
 }
 
 void drawball(struct ball_t * ball){
@@ -26,15 +45,46 @@ void drawball(struct ball_t * ball){
     printf("o");
 }
 
-int8_t checkEdge(struct ball_t * ball, int8_t x1 , int8_t y1,int8_t x2,int8_t y2){
+int8_t checkWallCollision(struct ball_t * ball, int8_t x1 , int8_t y1 ,int8_t x2 ,int8_t y2){
+    int8_t i = 0;
 
-    if ( ball->x <= x1+1 || ball->x >= x2-1 ){
-            ball->dx *=-1;
-            return 1;
+    // Check for collision with top wall
+    if (ball->x <= x1+1 && ball->vec.y > 0){ // if ball comes from left
+        rotat(&ball->vec,-128);
+        i = 1;
+    } else if (ball->x <= x1+1 && ball->vec.y < 0){ // if ball comes from right
+        rotat(&ball->vec,128);
+        i = 1;
     }
-    if ( ball->y <= y1+1 || ball->y >= y2 ){
-            ball->dy *=-1;
-            return 1;
+
+    // Check for collision with bottom wall
+    else if (ball->x >= x2-1 && ball->vec.y > 0){ // if ball comes from left
+        rotat(&ball->vec,128);
+        i = 1;
+    } else if (ball->x >= x2-1 && ball->vec.y < 0){ // if ball comes from right
+        rotat(&ball->vec,-128);
+        i = 1;
     }
-    return 0;
+
+
+    // Check for collision with right wall
+    if (ball->y <= y1+1 && ball->vec.x > 0){ // if ball comes from above
+        rotat(&ball->vec,128);
+        i = 1;
+    } else if (ball->y <= y1+1 && ball->vec.x < 0){ // if ball comes from bellow
+        rotat(&ball->vec,-128);
+        i = 1;
+    }
+
+    // Check for collision with left wall
+    else if (ball->y >= y2-1 && ball->vec.x > 0){ // if ball comes from above
+        rotat(&ball->vec,-128);
+        i = 1;
+    } else if (ball->y >= y2-1 && ball->vec.x < 0){ // if ball comes from bellow
+        rotat(&ball->vec,128);
+        i = 1;
+    }
+
+
+    return i;
 }
