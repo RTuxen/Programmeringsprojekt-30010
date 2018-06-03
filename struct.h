becfbc9bbd5974ff03b5e4ee33a272eb9d6b87e5
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-03 14:05:23 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -3,13 +3,38 @@
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" // Input/output library for this course
 
-struct ball_t {
-    int32_t x, y, dx, dy;
-};
-
 struct vector_t {
     int32_t x, y;
 };
 
+struct ball_t {
+    int32_t x;
+    int32_t y;
+    struct vector_t vec;
+};
+
+
+
+
+// theoretical structs
+struct Player {
+	int32_t x;
+	int32_t y;
+	int8_t points;
+	int8_t lifes;
+	int8_t level;
+};
+
+struct Block {
+	int8_t lifes;
+	int8_t x;
+	int8_t y;
+};
+
+struct Level {
+	struct Block blocks[128];
+	int8_t lifes;
+	int8_t wall[4];
+};
 
 #endif /*_STRUCT_H_*/
