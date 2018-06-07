a2d738d16bc437cae4ee4c5922114cd270b0fb62
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-07 10:54:57 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" // Input/output library for this course
-
 #include <string.h>
-#include <stdio.h>
 
 #include "ansi.h"
 #include "LUT.h"
@@ -10,12 +8,13 @@
 #include "gamePhysics.h"
 #include "IO.h"
 #include "timer.h"
-
+#include "display.h"
+#include "charset.h"
 
 #define X1 1
 #define Y1 1
-#define X2 24
-#define Y2 80
+#define X2 33
+#define Y2 125
 
 int main(void)
     {
@@ -27,37 +26,111 @@ int main(void)
     /***************************/
 	/* Initialization          */
 	/***************************/
-        struct ball_t bold;
-        int32_t timer=0;
-        int16_t hitcounter=0;
+
+
+
+
+
+    /***************************/
+	/* LCD slideshow test      */
+	/***************************/
 /*
-        initbold(&bold,12,10,1,1);
-        box(X1,Y1,X2,Y2,1);
-        minibox(X1,Y1,X2,Y2,0, hitcounter);
-        drawball(&bold);
+        uint8_t buffer[512];
+        initTimer100Hz();
+        initDisplay(buffer);
+        lcd_write_string(buffer,"HEJ MED oasd k ei jfj ud ke jd", 2, 0);
+        lcd_write_string(buffer,"HEJ MED oasd k ei jfj ud ke jd", 2, 1);
+        lcd_write_string(buffer,"HEJ MED oasd k ei jfj ud ke jd", 2, 2);
+        lcd_write_string(buffer,"Hokay", 2, 3);
+
+        initADC();
+        while(1) {
+            lcd_updateline(buffer, 3);
+        }
 */
 
 
-        clrscr();
-        initJoystick();
-        initLED();
-        initTimer100Hz();
+
+
+    /***************************/
+	/* LCD - Dreje håndtag     */
+	/***************************/
+ /*   uint16_t val1, val2;
+    uint8_t buffer[512];
+    char str1[30], str2[30];
+    initADC();
+    initDisplay(buffer);
+    while (0) {
+        val1 = readADC_pa0();
+        val2 = readADC_pa1();
+        sprintf(str1, "Value 1 = %4d", val1);
+        sprintf(str2, "Value 2 = %4d", val2);
+        lcd_write_string(buffer, str1, 1, 0);
+        lcd_write_string(buffer, str2, 1, 1);
+    }
+*/
+
+
+	/***************************/
+	/* Stopwatch Joystick test */
+	/***************************/
+/*
         uint8_t k = 0;
-        stopWatchWindow(10, 10, 15, 38, "Stop Watch", 1);
+        initJoystick();
+        initTimer100Hz();
+        clrscr();
+        stopWatchWindow(10, 10, "Stop Watch", 1);
+        int s = 0;
         while (1) {
+            if (s == tid.seconds){
             gotoxy(11,30);
             printTid();
             if (k != readJoyStick()){
                 k = readJoyStick();
                 setLed(k);
-                if (k) {
+                if (k == 4) {
                     gotoxy(12,30);
                     stopWatchControl(readJoyStick());
+                } else if (k == 8){
+                    gotoxy(13,30);
+                    stopWatchControl(readJoyStick());
+                } else if (k == 16){
+                    gotoxy(11,30);
+                    printf("0:0:00");
+                    stopWatchControl(readJoyStick());
+                } else if(k == 2){
+                    stopWatchControl(readJoyStick());
                 }
+
+            }
+            s = tid.seconds;
             }
         }
+*/
+
+	/***************************/
+	/* Stopwatch keyboard test */
+	/***************************/
+/*
+    gotoxy(20,0);
+    saveCursor();
+        while (1) {
+            gotoxy(11,30);
+            printTid();
+            getSavedCursor();
+            printf("\n");
+            textReturn(20);
+            printf("\n");
+            saveCursor();
+
+        }
+
+*/
 
 
+	/***************************/
+	/* Joystick LED test	   */
+	/***************************/
  /*
         initJoystick();
         initLED();
@@ -82,13 +155,24 @@ int main(void)
 	/***************************/
 	/* Main Loop	           */
 	/***************************/
-        while(0){
-        if (++timer==130000ul){
+
+        struct ball_t bold;
+        int32_t timerCount=0;
+        int16_t hitcounter=0;
+        clrscr();
+
+        initbold(&bold,12,10,1,1);
+        box(X1,Y1,X2,Y2,1);
+        minibox(X1,Y1,X2,Y2,0, hitcounter);
+        drawball(&bold);
+
+        while(1){
+        if (++timerCount==130000ul){
             hitcounter += checkWallCollision(&bold,X1,Y1,X2,Y2);
             minibox(X1 ,Y1 ,X2 ,Y2 ,0, hitcounter);
             update(&bold,X1,Y1,X2,Y2);
             drawball(&bold);
-            timer=0;
+            timerCount=0;
         }
 
         // Stops loop
