9dc1ffb5c58f6138ac9bef795083db81464938d8
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-07 10:54:41 +0200

    Update timer.c

--- src/timer.c
+++ src/timer.c
@@ -1,35 +1,31 @@
-#include "stm32f30x_conf.h" // STM32 config
-#include "30010_io.h" // Input/output library for this course
-
-#include "struct.h"
 #include "timer.h"
 
-void initTimer100Hz(){
-RCC->APB1ENR |= RCC_APB1Periph_TIM2; // Enable clock line to timer 2;
- TIM2->CR1 = 0x0000; // Configure timer 2 - disabled
- TIM2->ARR = 0x00009C3F; // Set reload value to 39999
- TIM2->PSC = 0x000F; // Set prescale value to 15
- TIM2->CR1 = 0x0001; // Configure timer 2 - enabled
- TIM2->DIER |= 0x0001; // Enable timer 2 interrupts
+volatile uint8_t flag;
+volatile uint8_t slide;
 
- NVIC_SetPriority(TIM2_IRQn, 0000); // Set interrupt priority
- NVIC_EnableIRQ(TIM2_IRQn); // Enable interrupt
+void initTimer100Hz(){
+    RCC->APB1ENR |= RCC_APB1Periph_TIM2; // Enable clock line to timer 2;
+    TIM2->CR1 = 0x0000; // Configure timer 2 - disabled
+    TIM2->ARR = 0x00009C3F; // Set reload value to 39999
+    TIM2->PSC = 0x000F; // Set prescale value to 15
+    TIM2->CR1 = 0x0001; // Configure timer 2 - enabled
+    TIM2->DIER |= 0x0001; // Enable timer 2 interrupts
+    slide = 0; //FlagEnable
+    NVIC_SetPriority(TIM2_IRQn, 0000); // Set interrupt priority
+    NVIC_EnableIRQ(TIM2_IRQn); // Enable interrupt
 }
 
 
 void TIM2_IRQHandler(void){
     tid.centiseconds++;
-    TIM2->SR &= ~0x0001; //Clear interrupt bit
-}
-
-
-void printTid(){
-    uint8_t en;
+    slide++;
+    if (slide >= 5) {
+        flag = 1;
+        slide = 0;
+    }
     if (tid.centiseconds >= 100) {
         tid.seconds++;
         tid.centiseconds = 0;
-        en = 1;
-
     }
     if (tid.seconds >= 60) {
         tid.minutes++;
@@ -40,19 +36,25 @@ void printTid(){
         tid.minutes=0;
     }
 
-    if (en) {
-        NVIC_DisableIRQ(TIM2_IRQn);
-        int8_t s = tid.seconds;
-        int8_t m = tid.minutes;
-        int8_t h = tid.hours;
-        NVIC_EnableIRQ(TIM2_IRQn); // Enable interrupt
-        printf("%d:%d:%d", h, m, s);
-        en = 0;
-    }
+    TIM2->SR &= ~0x0001; //Clear interrupt bit
 }
 
-void printSplit (int8_t c, int8_t s, int8_t m, int8_t h){
-    printf("%d:%d:%d.%d", h, m, s,c);
+
+void printTid(){
+    uint8_t s,m,h;
+
+        NVIC_DisableIRQ(TIM2_IRQn);
+        s = tid.seconds;
+        m = tid.minutes;
+        h = tid.hours;
+        NVIC_EnableIRQ(TIM2_IRQn); // Enable interrupt
+        printf("%d:%02d:%02d", h, m, s);
+
+
+}
+
+void printSplit (uint8_t c, uint8_t s, uint8_t m, uint8_t h){
+    printf("%d:%02d:%02d.%02d", h, m, s, c);
 }
 
 
@@ -86,4 +88,12 @@ void stopWatchControl(int8_t value){
         tid.minutes = 0;
         tid.hours = 0;
     }
+}
+
+uint8_t get_flag() {
+    if (flag) {
+        flag = 0;
+        return 1;
+    }
+    return 0;
 }
