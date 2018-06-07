41e8f4a9c901209c76324dbbdf52a51bda61ee2f
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-07 10:58:48 +0200

    Update IO.c

--- src/IO.c
+++ src/IO.c
@@ -1,22 +1,18 @@
-#include "stm32f30x_conf.h" // STM32 config
-#include "30010_io.h" // Input/output library for this course
-
-#include "struct.h"
 #include "IO.h"
 
 
 
 void initJoystick(){
-RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
-RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
-RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
 
 
 
 	/***************************/
 	/* right joystick	       */
 	/***************************/
-// Set pin PC0 to input
+    // Set pin PC0 to input
     GPIOC->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
     GPIOC->MODER |= (0x00000000 << (0 * 2));
     // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
@@ -41,7 +37,7 @@ RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
 	/***************************/
 	/* left joystick	       */
 	/***************************/
-// Set pin PC1 to input
+    // Set pin PC1 to input
     GPIOC->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
     GPIOC->MODER |= (0x00000000 << (1 * 2));
     // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
@@ -146,9 +142,9 @@ RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
 }
 
 void initLED(){
-RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
-RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
-RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
 
  	/***************************/
 	/* Red LED	               */
@@ -193,7 +189,7 @@ RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
     GPIOA->ODR |= (0x0001 << 9); //Set pin PB0 to high
 
 
- // Set pin PA9 to output
+    // Set pin PA9 to output
     GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
     GPIOA->OSPEEDR |= (0x00000002 << (9 * 2));
     // set speed register (0x01 - 10MHz, 0x02 - 2 MHz, 0x03 - 50 MHz)
@@ -203,7 +199,6 @@ RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
     GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
     GPIOA->MODER |= (0x00000001 << (9 * 2));
     // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
-
 }
 
 
@@ -269,4 +264,55 @@ void setLed(int8_t value){ // LED turns on at low
         GPIOA->ODR &= ~BLUE;
     }
 
+}
+
+void initADC() {
+
+    RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
+
+    // Set pin PA0 to input
+    GPIOA->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
+    GPIOA->MODER |= (0x00000000 << (0 * 2));
+    // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+    GPIOA->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
+
+   // Set pin PA1 to input
+    GPIOA->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
+    GPIOA->MODER |= (0x00000000 << (1 * 2));
+    // Set mode register (0x00 - Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+    GPIOA->PUPDR &= ~(0x00000003 << (1 * 2)); // Clear push/pull register
+
+    RCC->CFGR2 &= ~RCC_CFGR2_ADCPRE12;     // Clear ADC12 prescaler bits
+    RCC->CFGR2 |= RCC_CFGR2_ADCPRE12_DIV6; // Set ADC12 prescaler to 6
+    RCC->AHBENR |= RCC_AHBPeriph_ADC12;    // Enable clock for ADC12
+
+    ADC1->CR = 0x00000000;          // Clear CR register
+    ADC1->CFGR &= 0xFDFFC007;       // Clear ADC1 config register
+    ADC1->SQR1 &= ~ADC_SQR1_L;      // Clear regular sequence register 1
+
+    ADC1->CR |= 0x10000000;             // Enable internal ADC voltage regulator
+    for (int i = 0 ; i < 1000 ; i++) {} // Wait for about 16 microseconds
+
+    ADC1->CR |= 0x80000000;             // Start ADC1 calibration
+    while (!(ADC1->CR & 0x80000000));   // Wait for calibration to finish
+    for (int i = 0 ; i < 100 ; i++) {}  // Wait for a little while
+
+    ADC1->CR |= 0x00000001;             // Enable ADC1 (0x01 - Enable, 0x02 - Disable)
+    while (!(ADC1->ISR & 0x00000001));  // Wait until ready
+}
+
+uint16_t readADC_pa0() { //Channel 1
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_1Cycles5);
+    ADC_StartConversion(ADC1);                          // Start ADC read
+    while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == 0); // Wait for ADC read
+    uint16_t x = ADC_GetConversionValue(ADC1);          // Read the ADC value
+    return x;
+}
+
+uint16_t readADC_pa1() { //Channel 2
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_1Cycles5);
+    ADC_StartConversion(ADC1);                          // Start ADC read
+    while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == 0); // Wait for ADC read
+    uint16_t x = ADC_GetConversionValue(ADC1);          // Read the ADC value
+    return x;
 }
