fb8d370d25413796fa796a7b9d33f1393ab35123
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-08 09:10:59 +0200

    Update gamePhysics.h

--- gamePhysics.h
+++ gamePhysics.h
@@ -2,12 +2,15 @@
 #define _GAMEPHYSICS_H_
 
 #include "struct.h"
-
+#include "IO.h"
+#include "graphics.h"
 
 void initbold (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy);
-void update(struct ball_t * ball,int8_t x1 , int8_t y1,int8_t x2,int8_t y2);
-void drawball(struct ball_t * ball);
+void initPlayer(struct player_t * striker, uint8_t x, uint8_t y);
+uint8_t updateBallPos(struct ball_t * ball,struct player_t *striker, int8_t x1 , int8_t y1,int8_t x2,int8_t y2);
+void updatePlayerPos(struct player_t *striker);
 int8_t checkWallCollision(struct ball_t * ball, int8_t x1 , int8_t y1,int8_t x2,int8_t y2);
+uint8_t checkPlayerCollision(struct ball_t * ball, struct player_t * striker);
 
 
 #endif /* _GAMEPHYSICS_H_ */
