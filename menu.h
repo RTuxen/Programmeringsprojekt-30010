84d12298e8e4dc0dfe9a161db1a5f957016de7a2
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-09 12:51:14 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,23 +1,23 @@
 #ifndef _MENU_H_
 #define _MENU_H_
+
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" // Input/output library for this course
 #include "graphics.h"
 #include "IO.h"
+#include "ansi.h"
 
 void chooseMenuOptions();
 void chooseLevel();
 void chooseOptions();
 void chooseHighscore();
 void chooseHelp();
+uint8_t changeSpeed();
 
-int8_t drawMenuWindow(int8_t x1, int8_t y1, int8_t style);
-void drawPlayWindow(int8_t x1, int8_t y1, int8_t style);
-void drawOptionWindow(int8_t x1, int8_t y1, int8_t style);
-void drawHighscoreWindow(int8_t x1, int8_t y1, int8_t style, uint8_t *highscores);
+void drawMenuWindow();
+void drawPlayWindow();
+void drawOptionWindow();
+void drawHighscoreWindow(uint8_t *highscores);
 void drawHelpWindow();
-
-
-
 
 #endif
