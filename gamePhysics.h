beb4a7c2931cffde1322e3b832d695d6d4dfe1f1
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-11 16:42:04 +0200

    Update gamePhysics.h

--- gamePhysics.h
+++ gamePhysics.h
@@ -5,16 +5,21 @@
 #include "IO.h"
 #include "graphics.h"
 #include "levels.h"
+#include "display.h"
+#include "menu.h"
 
-void initbold (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy);
-void initPlayer(struct player_t * striker, uint16_t x, uint16_t y);
+void initBall (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy);
+void initPlayer(struct player_t * striker, uint16_t x, uint16_t y, uint8_t levelNum);
+void playGame(uint8_t levelNum);
 void initLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level);
+void restartLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level);
+
 uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct level_t *level);
 void updatePlayerPos(struct player_t *striker);
+
 uint16_t checkWallCollision(struct ball_t * ball);
 uint16_t checkPlayerCollision(struct ball_t * ball, struct player_t * striker);
-
-uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level);
+uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level, struct player_t * striker);
 
 
 #endif /* _GAMEPHYSICS_H_ */
