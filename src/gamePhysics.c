b5ed9b317b1595d3a1d9da127c7404e227a88f6b
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-11 16:43:40 +0200

    Update gamePhysics.c

--- src/gamePhysics.c
+++ src/gamePhysics.c
@@ -6,35 +6,58 @@
 #include "struct.h"
 #include "gamePhysics.h"
 
-void initbold (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy){
+void initBall (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy){
     ball->x = x << 14;
     ball-> y = y << 14;
     ball->vec.x = FIX14_MULT(dx << 14, 5000);
     ball->vec.y = FIX14_MULT(dy << 14, 5000);
 }
 
-void initPlayer(struct player_t * striker, uint16_t x, uint16_t y){
+void initPlayer(struct player_t * striker, uint16_t x, uint16_t y, uint8_t levelNum){
     striker->x = x;
     striker->y = y;
-    striker->lifes = 3;
-    striker->level = 0;
+    striker->lives = 1;
+    striker->level = levelNum;
     striker->points = 0;
 }
 
+void playGame(uint8_t levelNum){
+    struct ball_t ball;
+    struct player_t striker;
+    struct level_t bane;
+    static uint8_t buffer[512];
+    int16_t slut=0;
+    int32_t timerCount=0;
+
+    //Initialisere LCD
+    initBall(&ball,X2-7,Y2/2,0,0);//Initialisere bolden
+    initPlayer(&striker,X2-5,Y2/2, levelNum-1);//Initialisere strikeren
+    initLevel(&ball,&striker,&bane);//Initialisere blokkene
+    initDisplay(buffer);
+    while(1){
+            if (++timerCount==100000ul){
+                updatePlayerPos(&striker);//Updatere strikeren
+                slut = updateBallPos(&ball,&striker,&bane);//Updatere bolden og blokkene
+                LCD_Printer(striker.level, striker.lives, striker.points, buffer);//Viser level, liv og point på LCD
+                if (slut){
+                    chooseGameOver(striker.points, levelNum, buffer);//Stopper spillet, hvis liv = 0
+                }
+                timerCount=0;
+            }
+        }
+
+}
 
 void initLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level){
         clrscr();
         uint8_t i;
 
-        initbold(ball,X2-7,Y2/2,0,0);
-        initPlayer(striker,X2-5,Y2/2);
-        initJoystick();
-        level->lifes = 0;
+        level->lives = 0;
 
         for (i = 0; i < 32; i++) {
-            level->blocks[i] = Level1[i];
-            if (level->blocks[i].lifes > 0) {
-                level->lifes += level->blocks[i].lifes;
+            level->blocks[i] = Level[striker->level][i];
+            if (level->blocks[i].lives > 0) {
+                level->lives += level->blocks[i].lives;
             }
         }
         drawWalls();
@@ -43,10 +66,11 @@ void initLevel(struct ball_t *ball, struct player_t *striker, struct level_t *le
         drawPlayer(striker);
 }
 
+
 void restartLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level){
         clrscr();
 
-        initbold(ball,X2-7,Y2/2,0,0);
+        initBall(ball,X2-7,Y2/2,0,0);
         striker->x =X2-5;
         striker->y = Y2/2;
 
@@ -61,19 +85,21 @@ void restartLevel(struct ball_t *ball, struct player_t *striker, struct level_t 
 
 
 uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct level_t *level){
-        gotoxy(ball->x >> 14,ball->y >> 14);
-        printf(" ");
         uint16_t playerhit = checkPlayerCollision(ball,striker);
         uint16_t wallhit = checkWallCollision(ball);
         checkBlockCollision(ball,level,striker);
+
+        static int32_t oldx, oldy;
+        oldx= ball->x;
+        oldy = ball->y;
 
 
         if ( ball->vec.x == 0 && ball->vec.y == 0){
             ball->x = (striker->x-2) << 14;
             ball->y = (striker->y) << 14;
             if ( readJoyStickContinous() == 16){
-                ball->vec.x = -FIX14_MULT(1 << 14, 5000);
-                ball->vec.y = FIX14_MULT(1 << 14, 5000);
+                ball->vec.x = -FIX14_MULT(1 << 14, 10000);
+                ball->vec.y = FIX14_MULT(1 << 14, 10000);
             }
 
         }
@@ -81,6 +107,7 @@ uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct lev
 
         ball->x += ball->vec.x;
         ball->y += ball->vec.y;
+
 
 
         // Makes sure ball doesn't exceed bounds with larger vectors
@@ -98,11 +125,11 @@ uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct lev
                 ball->y = Y1+1;
             }
         } else if (wallhit ==2){ // Ball dead
-            if ( striker->lifes > 1){
-                    striker->lifes -=1;
+            if ( striker->lives > 1){
+                    striker->lives -=1;
                     restartLevel(ball,striker,level);
             } else{
-                striker->lifes -=1;
+                striker->lives -=1;
                 return 1;
             }
         }
@@ -110,31 +137,56 @@ uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct lev
         if (playerhit){ // If ball hits player
             ball->x = (striker->x-1) << 14;
         }
-
-        drawball(ball);
+        if ( oldx != ball->x || oldy != ball->y){
+            gotoxy(oldx >> 14,oldy >> 14);
+            printf(" ");
+            drawball(ball);
+        }
         return 0;
 }
 
 void updatePlayerPos(struct player_t *striker){
-    uint16_t oldx = striker->x, oldy = striker->y;
-    gotoxy(oldx,oldy-7);
-    printf("               ");
+    fgcolor(1);
+    static uint16_t oldPlayerx,oldPLayery;
+    oldPlayerx = striker->x;
+    oldPLayery = striker->y;
 
     if (readJoyStickContinous() == 4){ //Left
-        if(oldy-3 >= Y1+8){
+        if(oldPLayery-3 >= Y1+8){
+            gotoxy(oldPlayerx,oldPLayery+7-2);
+            printf("   ");
+            moveCursor('D',15+3);
+            printf("%c%c%c",219,219,219);
             striker->y -= 3;
-        } else if(oldy-1 >= Y1+8){
+        } else if(oldPLayery-1 >= Y1+8){
+            gotoxy(oldPlayerx,oldPLayery+7-1);
+            printf(" ");
+            moveCursor('D',15+1);
+            printf("%c",219);
             striker->y -=1;
         }
     } else if (readJoyStickContinous() == 8){ // Right
-        if (oldy+3 <= Y2-7){
+        if (oldPLayery+3 <= Y2-7){
+            gotoxy(oldPlayerx,oldPLayery-7);
+            printf("   ");
+            moveCursor('C',15-3);
+            printf("%c%c%c",219,219,219);
             striker->y += 3;
-        } else if (oldy+1 <= Y2-7){
+        } else if (oldPLayery+1 <= Y2-7){
+            gotoxy(oldPlayerx,oldPLayery-7);
+            printf(" ");
+            moveCursor('C',15-1);
+            printf("%c",219);
             striker->y +=1;
         }
     }
 
-    drawPlayer(striker);
+
+//    if( oldPlayerx != striker->x || oldPLayery != striker->y){
+//        gotoxy(oldPlayerx,oldPLayery-7);
+//        printf("               ");
+//        drawPlayer(striker);
+//    }
 }
 
 uint16_t checkWallCollision(struct ball_t * ball){
@@ -240,7 +292,7 @@ uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level, struct 
 	struct block_t * blocks = level->blocks;
 
 	for (i = 0; i < 32; i++) {
-		if (blocks[i].lifes > 0) {
+		if (blocks[i].lives > 0) {
 			hit = 0;
 
             // Check if ball hits from top or bottom
@@ -253,11 +305,10 @@ uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level, struct 
 
 			if (hit == 1) {
 
-				blocks[i].lifes--;
+				blocks[i].lives--;
 				drawBlock(blocks[i]);
-				level->lifes--;
+				level->lives--;
 				striker->points++;
-
 			}
 
 		}
