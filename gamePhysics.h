5e6797d0ae33c6a92e3daa539afb187f6f22e1de
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-13 11:33:08 +0200

    Update gamePhysics.h

--- gamePhysics.h
+++ gamePhysics.h
@@ -1,25 +1,32 @@
 #ifndef _GAMEPHYSICS_H_
 #define _GAMEPHYSICS_H_
 
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
 #include "struct.h"
 #include "IO.h"
+#include "ansi.h"
+#include "LUT.h"
 #include "graphics.h"
 #include "levels.h"
 #include "display.h"
 #include "menu.h"
+#include "timer.h"
 
-void initBall (struct ball_t* ball, int32_t x, int32_t y, int32_t dx, int32_t dy);
-void initPlayer(struct player_t * striker, uint16_t x, uint16_t y, uint8_t levelNum);
-void playGame(uint8_t levelNum);
-void initLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level);
+void initGameState(struct game_state_t* gs);
+void initBall (struct ball_t* ball, int32_t x, int32_t y, int32_t speed);
+void initPlayer(struct player_t * striker, uint16_t x, uint16_t y);
+void initLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level, struct game_state_t* gs);
+void playGame(struct game_state_t* gs);
 void restartLevel(struct ball_t *ball, struct player_t *striker, struct level_t *level);
 
-uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct level_t *level);
+uint16_t updateBallPos(struct ball_t * ball,struct player_t *striker, struct level_t *level, struct game_state_t * gs);
 void updatePlayerPos(struct player_t *striker);
 
 uint16_t checkWallCollision(struct ball_t * ball);
 uint16_t checkPlayerCollision(struct ball_t * ball, struct player_t * striker);
-uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level, struct player_t * striker);
+uint16_t checkBlockCollision(struct ball_t* ball, struct level_t* level, struct game_state_t * gs);
+void bossKey();
 
 
 #endif /* _GAMEPHYSICS_H_ */
