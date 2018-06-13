c3d28f07528a5396bbdc76745ffbc6f081e5ff07
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-13 11:36:14 +0200

    Update display.c

--- src/display.c
+++ src/display.c
@@ -5,15 +5,15 @@ void initDisplay(uint8_t *buffer){
     lcd_push_buffer(buffer);
 }
 
- void LCD_Printer(uint8_t level, uint8_t lives, uint8_t points, uint8_t* buffer){
-    uint16_t val1 = level+1, val2 = lives, val3 = points;
+void LCD_Printer(struct game_state_t* gs){
+    uint16_t val1 = gs->currentlevel, val2 = gs->lives, val3 = gs->points;
     char str1[30], str2[30], str3[30];
     sprintf(str1, "Level   = %4d", val1);
     sprintf(str2, "Lives   = %4d", val2);
     sprintf(str3, "Points  = %4d", val3);
-    lcd_write_string(buffer, str1, 1, 0);
-    lcd_write_string(buffer, str2, 1, 1);
-    lcd_write_string(buffer, str3, 1, 2);
+    lcd_write_string(gs->buffer, str2, 1, 1);
+    lcd_write_string(gs->buffer, str3, 1, 2);
+    lcd_write_string(gs->buffer, str1, 1, 0);
 }
 
 void lcd_write_string(uint8_t *buffer, char *text, uint8_t slice, uint8_t line){ //Antager slice [0,128] og line[0,3]
