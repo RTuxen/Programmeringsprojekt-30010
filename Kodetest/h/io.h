faf8b8227e9f9488b564417184e25c867d08f402
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-14 17:05:46 +0200

    Create io.h

---
+++ Kodetest/h/io.h
@@ -0,0 +1,22 @@
+#ifndef IO66_H
+#define IO66_H
+
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+
+#include "struct.h"
+#define RED (0x0001 << 4)
+#define GREEN (0x0001 << 7)
+#define BLUE (0x0001 << 9)
+
+void initJoystick();
+int8_t readJoyStick();
+int8_t readJoyStickContinous();
+void initLED();
+void setLed(int8_t value);
+void initADC();
+uint16_t readADC_pa0();
+uint16_t readADC_pa1();
+uint8_t readKeyboard();
+
+#endif	// IO66_H
