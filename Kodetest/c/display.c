900e9cb230a63e40a70306124c94840254a5a8ba
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-14 17:02:29 +0200

    Create display.c

---
+++ Kodetest/c/display.c
@@ -0,0 +1,57 @@
+#include "display.h"
+
+void initDisplay(uint8_t *buffer){
+    memset(buffer, 0x00, 512); // Sets eah eleen of the buffer to 0xAA
+    lcd_push_buffer(buffer);
+}
+
+void LCD_Printer(struct game_state_t* gs){
+    uint16_t val1 = gs->currentlevel, val2 = gs->lives, val3 = gs->points;
+    char str1[30], str2[30], str3[30];
+    sprintf(str1, "Level   = %4d", val1);
+    sprintf(str2, "Lives   = %4d", val2);
+    sprintf(str3, "Points  = %4d", val3);
+    lcd_write_string(gs->buffer, str2, 1, 1);
+    lcd_write_string(gs->buffer, str3, 1, 2);
+    lcd_write_string(gs->buffer, str1, 1, 0);
+}
+
+void lcd_write_string(uint8_t *buffer, char *text, uint8_t slice, uint8_t line){ //Antager slice [0,128] og line[0,3]
+    uint8_t textlenght = strlens(text);
+    int8_t i,j;
+    int16_t k =slice+line*128-1;
+
+
+    for (i = 0; i< textlenght; i++){
+            for (j=0; j <= 4; j++){
+                if (7*i+k+j >= 128*line+128) {
+                    break;
+                }
+                buffer[7*i+k+j]= character_data[text[i]-0x20][j];
+            }
+    }
+    lcd_push_buffer(buffer);
+}
+
+void lcd_updateline(uint8_t *buffer, uint8_t line){
+    uint16_t n = line * 128;
+    if (get_flag()){
+        memmove(buffer + n, buffer + 1 + n, 127);
+        buffer[127+n] = buffer[0 + n];
+        lcd_push_buffer(buffer);
+    }
+}
+
+void lcd_update(uint8_t *buffer){
+    int8_t i = 0;
+    uint16_t n;
+    if (get_flag()){
+        for (i = 0; i < 4; ++i) {
+            n = i * 128;
+            memmove(buffer + n, buffer + 1 + n, 127);
+            buffer[127+n] = buffer[0 + n];
+        }
+        lcd_push_buffer(buffer);
+
+    }
+}
