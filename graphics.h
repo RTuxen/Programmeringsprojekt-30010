fdf63d3d992e121a0c8f2f120aae151a2605b345
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-15 15:33:20 +0200

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,9 +1,9 @@
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+
 #ifndef _GRAPHICS_H_
 #define _GRAPHICS_H_
 
-
-#include <string.h>
-#include "LUT.h"
 #include "struct.h"
 #include "ansi.h"
 #include "levels.h"
@@ -13,8 +13,10 @@ void window (uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, char str[], uint8_t
 void box (uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, int8_t style);
 void minibox (int8_t x1, int8_t y1, int8_t x2, int8_t y2, int8_t style, uint8_t n);
 void stopWatchWindow(int8_t x1, int8_t y1, char str[], int8_t style);
-void textReturn(int8_t len);
-void compareString(char * text);
+
+//void textReturn(int8_t len);
+//void compareString(char * text);
+
 void commandList();
 void userGuide();
 
@@ -23,6 +25,10 @@ void drawball(struct ball_t * ball);
 void drawPlayer(struct player_t *striker);
 void drawBlock(struct block_t block);
 void drawBlockMap(struct block_t *bricks);
-
+void printfallObject(struct fallingObject_t *fallObject);
+void drawWelcomeMessage();
+void drawHighscoreMessage();
+void drawOptionMessage();
+void drawHelpMessage();
 
 #endif /* _GRAPHICS_H_ */
