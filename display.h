62a047d7d81f8ad429af772549db171ec2640b8e
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-15 15:32:50 +0200

    Update display.h

--- display.h
+++ display.h
@@ -1,14 +1,14 @@
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+
 #ifndef _DISPLAY_H_
 #define _DISPLAY_H_
 
-#include "stm32f30x_conf.h" // STM32 config
-#include "30010_io.h" // Input/output library for this course
 #include <string.h>
 #include "struct.h"
 #include "charset.h"
-#include "ansi.h"
 #include "timer.h"
-#include "graphics.h"
+
 
 void initDisplay(uint8_t *buffer);
 void LCD_Printer(struct game_state_t* gs);
