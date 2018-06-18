d8f6a47a48c4eb01c19b57dbf0e03a52864b534a
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-18 13:52:07 +0200

    Create sound.h

---
+++ sound.h
@@ -0,0 +1,14 @@
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+
+#ifndef _SOUND_H_
+#define _SOUND_H_
+
+#include "struct.h"
+
+
+extern const uint16_t  MELODY[];
+extern const uint8_t TEMPO[];
+
+#endif	// _SOUND_H_
+
