0fffb87ab521f721a3ca8a29343d594dd528dc5d
Mathiasmkl <39878117+Mathiasmkl@users.noreply.github.com>
2018-06-18 11:28:01 +0200

    Add files via upload

--- Kodetest/timer.c
+++ Kodetest/timer.c
@@ -1,30 +1,28 @@
 #include "timer.h"
 
-volatile uint8_t flag = 0;       //Flag til LCD update
-volatile uint8_t slide = 0;      //Variabel til LCD update
-volatile uint8_t gameflag = 0;   //Flag til spil update
-volatile uint8_t updateGame = 0; //Variabel til spil update
+volatile uint8_t flag = 0;      //Flag til LCD update
+volatile uint8_t slide = 0;     //Variabel til LCD update
+volatile uint8_t gameflag = 0;  //Flag til spil update
+volatile uint8_t updateGame = 0;//Variabel til spil update
 
 void initTimer100Hz(){
     RCC->APB2ENR |= RCC_APB2Periph_TIM15; // Enable clock line to timer 15;
     TIM15->CR1 = 0x0000; // Configure timer 15 - disabled
-    //TIM15->ARR = 0x9C3F; // Set reload value to 39999
-    TIM15->ARR = 0x00000F9F; // Set reload value to 3999
+    TIM15->ARR = 0x9C3F; // Set reload value to 39999
     TIM15->PSC = 0x000F; // Set prescale value to 15
     TIM15->DIER |= 0x0001; // Enable timer 2 interrupts
 
-    NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0001); // Set interrupt priority
+    NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 0); // Set interrupt priority
     NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn); // Enable interrupt
 
     TIM15->CR1 |= 0x0001; // Configure timer 15 - enabled
 }
 
 void TIM1_BRK_TIM15_IRQHandler(void){
-
-    tid.milliseconds++;
+    tid.centiseconds++;
     slide++;
     updateGame++;
-    tid.joystickdebouncer++;
+    tid.soundtime++;
     if (updateGame >= 5){//Spillet updateres
         gameflag = 1;
         updateGame = 0;
@@ -33,9 +31,12 @@ void TIM1_BRK_TIM15_IRQHandler(void){
         flag = 1;
         slide = 0;
     }
-    if (tid.milliseconds >= 1000) {
+    if (tid.centiseconds >= 100) {
         tid.seconds++;
-        tid.milliseconds = 0;
+        tid.centiseconds = 0;
+    }
+    if (tid.soundtime >= 65000){
+        tid.soundtime = 0;
     }
 
     TIM15->SR &= ~0x0001; //Clear interrupt bit
@@ -47,7 +48,7 @@ void initTimerPWM(){
     TIM2->ARR = 0x000003E7; // Set reload value to 999
     TIM2->PSC = 0x000F; // Set prescale value to 15
     TIM2->CR1 = 0x0001; // Configure timer 2 - enabled
-
+    TIM2->DIER |= 0x0001; // Enable timer 2 interrupts
 
     TIM2->CCER &= ~TIM_CCER_CC3P; // Clear CCER register
     TIM2->CCER |= 0x00000001 << 8; // Enable OC3 output
@@ -57,16 +58,27 @@ void initTimerPWM(){
     TIM2->CCMR2 &= ~TIM_CCMR2_OC3PE;
     TIM2->CCMR2 |= TIM_OCPreload_Enable;
     TIM2->CCR3 = 500; // Set duty cycle to 50 %
+
+    NVIC_SetPriority(TIM2_IRQn, 0001); // Set interrupt priority
+    NVIC_EnableIRQ(TIM2_IRQn); // Enable interrupt
 }
 
+void setFreq(uint16_t freq) {
+ uint32_t reload = (64e6 / freq / 16) - 1;
+ TIM2->ARR = reload; // Set auto reload value
+ TIM2->CCR3 = reload/2; // Set compare register
+ TIM2->EGR |= 0x01;
+ }
 
 void TIM2_IRQHandler(void){
+    tid.hours++;
+
     TIM2->SR &= ~0x0001; //Clear interrupt bit
 }
 
 void setTimer(uint8_t difficulty, uint8_t levelspeed){
     TIM15->CR1 = 0x0000; // Configure timer 2 - disabled
-    TIM15->ARR = 0x00009C3F/(1+2*difficulty+levelspeed); // Set reload value to 3999 - VED GODT MAN IKKE MÃ… SKRIVE 0.25 og 0.5
+    TIM15->ARR = 0x00009C3F/(1+2*difficulty+levelspeed); // Set reload value to 3999 - VED GODT MAN IKKE MÅ SKRIVE 0.25 og 0.5
     TIM15->PSC = 0x000F; // Set prescale value to 15
     TIM15->CR1 = 0x0001; // Configure timer 2 - enabled
     TIM15->EGR |= 0x0001;
---
+++ Kodetest/timer.h
@@ -0,0 +1,17 @@
+#ifndef IO_H
+#define IO_H
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+#include "struct.h"
+
+void initTimer100Hz();
+void initTimerPWM();
+void setTimer(uint8_t difficulty, uint8_t levelspeed);
+void setFreq(uint16_t freq);
+void printTid();
+void stopWatchControl(int8_t value);
+void printSplit (uint16_t ms, uint8_t s, uint8_t m, uint8_t h);
+uint8_t get_flag();
+uint8_t get_game_flag();
+
+#endif	// IO_H
