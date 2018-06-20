a5c1279cb435523de78734b860ea1f851d1f046b
RTuxen <39836677+RTuxen@users.noreply.github.com>
2018-06-20 17:29:51 +0200

    Add files via upload

--- gamePhysics.h
+++ gamePhysics.h
@@ -5,24 +5,19 @@
 #define _GAMEPHYSICS_H_
 
 #include "struct.h"
-
 #include "menu.h"
 #include "LUT.h"
-
-
 
 int32_t moveBallX(struct ball_t * ball);
 int32_t moveBallY(struct ball_t * ball);
 
 void updateFallingObject(struct fallingObject_t *fallObject,struct level_t * level, struct player_t * striker, struct game_state_t* gs);
-
 void updateFallingObject2Players(struct fallingObject_t *fallObject, struct player_t * striker, struct player_t * striker2, struct game_state_t* gs);
-
 void updateBallPos(struct ball_t * ball,struct player_t *striker, struct level_t *level, struct game_state_t * gs,struct fallingObjectsType_t * fallObject_ptr);
 void updateBallPos2Players(struct ball_t * ball,struct player_t *striker,struct player_t * striker2, struct level_t *level, struct game_state_t * gs, struct fallingObjectsType_t * fallObject_ptr);
+
 uint16_t checkWallCollision(struct ball_t * ball,struct game_state_t *gs);
 void checkPlayerCollision(struct ball_t * ball, struct player_t * striker, struct game_state_t * gs);
 void checkBlockCollision(struct ball_t* ball, struct level_t* level, struct game_state_t * gs, struct fallingObjectsType_t *fallObject_ptr);
-
 
 #endif /* _GAMEPHYSICS_H_ */
